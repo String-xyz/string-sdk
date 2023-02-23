229bbc56ae8ae9b9441ffc28d314d38a1704cdbb
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-02-23 17:13:40 -0500

    STR-414 :: Avoid Events Duplication (#26)
    * avoid event duplication
    
    * fix tests

--- src/lib/services/apiClient.service.ts
+++ src/lib/services/apiClient.service.ts
@@ -1,314 +1,336 @@
-import axios from 'redaxios';
+import axios from "redaxios";
 
 // TODO: Fix timeout issue
 
-export function createApiClient({ apiKey, baseUrl, walletAddress }: ApiClientOptions): ApiClient {
-	let _userWalletAddress = walletAddress;
+export function createApiClient({ baseUrl }: ApiClientOptions): ApiClient {
+    let _userWalletAddress = "";
+    let apiKey = "";
 
-	const commonHeaders: any = {
-		'Content-Type': 'application/json',
-	};
+    const commonHeaders: any = {
+        "Content-Type": "application/json",
+    };
 
-	const httpClient = axios.create({
-		baseURL: baseUrl,
-		headers: commonHeaders,
-		withCredentials: true, // send cookies,
-		// fetch: fetchWithTimeout(10000),
-	});
+    const httpClient = axios.create({
+        baseURL: baseUrl,
+        headers: commonHeaders,
+        withCredentials: true, // send cookies,
+        // fetch: fetchWithTimeout(10000),
+    });
 
-	const setWalletAddress = (addr: string) => _userWalletAddress = addr;
+    const setWalletAddress = (addr: string) => (_userWalletAddress = addr);
+    const setApiKey = (key: string) => (apiKey = key);
 
-	async function createApiKey() {
-		const { data } = await httpClient.post<{ apiKey: string }>('/apikeys');
-		return data;
-	}
+    async function createApiKey() {
+        const { data } = await httpClient.post<{ apiKey: string }>("/apikeys");
+        return data;
+    }
 
-	async function getApiKeys(limit = 10) {
-		const { data } = await httpClient.get<ApiKeyResponse[]>('/apikeys', { params: { limit } });
-		return data;
-	}
+    async function getApiKeys(limit = 10) {
+        const { data } = await httpClient.get<ApiKeyResponse[]>("/apikeys", {
+            params: { limit },
+        });
+        return data;
+    }
 
-	async function validateApiKey(keyId: string) {
-		const { data } = await httpClient.post<{ Status: string }>(`/apikeys/${keyId}/approve`);
-		return data;
-	}
+    async function validateApiKey(keyId: string) {
+        const { data } = await httpClient.post<{ Status: string }>(`/apikeys/${keyId}/approve`);
+        return data;
+    }
 
-	async function requestLogin(walletAddress: string) {
-		setWalletAddress(walletAddress);
-		try {
-			const headers = { 'X-Api-Key': apiKey };
-			const { data } = await httpClient.get<{ nonce: string }>(`/login`, { params: { walletAddress: _userWalletAddress }, headers });
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function requestLogin(walletAddress: string) {
+        setWalletAddress(walletAddress);
+        try {
+            const headers = { "X-Api-Key": apiKey };
+            const { data } = await httpClient.get<{ nonce: string }>(`/login`, {
+                params: { walletAddress: _userWalletAddress },
+                headers,
+            });
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function createUser(nonce: string, signature: string, visitor?: VisitorData) {
-		const headers = { 'X-Api-Key': apiKey };
-		const body = {
-			nonce,
-			signature,
-			fingerprint: visitor
-		};
+    async function createUser(nonce: string, signature: string, visitor?: VisitorData) {
+        const headers = { "X-Api-Key": apiKey };
+        const body = {
+            nonce,
+            signature,
+            fingerprint: visitor,
+        };
 
-		try {
-			const { data } = await httpClient.post<{ authToken: AuthToken, user: User }>(`/users`, body, { headers });
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+        try {
+            const { data } = await httpClient.post<{
+                authToken: AuthToken;
+                user: User;
+            }>(`/users`, body, { headers });
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function updateUser(userId: string, update: UserUpdate) {
-		const request = () => httpClient.put<User>(`/users/${userId}`, update, {
-			headers: { 'X-Api-Key': apiKey },
-		});
+    async function updateUser(userId: string, update: UserUpdate) {
+        const request = () =>
+            httpClient.put<User>(`/users/${userId}`, update, {
+                headers: { "X-Api-Key": apiKey },
+            });
 
-		const { data } = await authInterceptor<{ data: User }>(request);
-		return data;
-	}
+        const { data } = await authInterceptor<{ data: User }>(request);
+        return data;
+    }
 
-	async function requestEmailVerification(userId: string, email: string) {
-		try {
-			const request = () => httpClient.get(`/users/${userId}/verify-email`, {
-				headers: { 'X-Api-Key': apiKey },
-				params: { email },
-				// timeout: 15 * 60 * 1000 // 15 minutes
-			});
+    async function requestEmailVerification(userId: string, email: string) {
+        try {
+            const request = () =>
+                httpClient.get(`/users/${userId}/verify-email`, {
+                    headers: { "X-Api-Key": apiKey },
+                    params: { email },
+                    // timeout: 15 * 60 * 1000 // 15 minutes
+                });
 
-			await authInterceptor(request);
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+            await authInterceptor(request);
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function loginUser(nonce: string, signature: string, visitor?: VisitorData) {
-		const headers = { 'X-Api-Key': apiKey };
-		const body = {
-			nonce,
-			signature,
-			fingerprint: visitor
-		};
+    async function loginUser(nonce: string, signature: string, visitor?: VisitorData) {
+        const headers = { "X-Api-Key": apiKey };
+        const body = {
+            nonce,
+            signature,
+            fingerprint: visitor,
+        };
 
-		try {
-			const { data } = await httpClient.post<{ authToken: AuthToken, user: User }>(`/login/sign`, body, { headers });
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+        try {
+            const { data } = await httpClient.post<{
+                authToken: AuthToken;
+                user: User;
+            }>(`/login/sign`, body, { headers });
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function refreshToken(walletAddress: string) {
-		const headers = { 'X-Api-Key': apiKey };
-		try {
-			const { data } = await httpClient.post<{ authToken: AuthToken, user: User }>(`/login/refresh`, { walletAddress }, { headers });
-			console.log(' - Token was refreshed')
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function refreshToken(walletAddress: string) {
+        const headers = { "X-Api-Key": apiKey };
+        try {
+            const { data } = await httpClient.post<{
+                authToken: AuthToken;
+                user: User;
+            }>(`/login/refresh`, { walletAddress }, { headers });
+            console.log(" - Token was refreshed");
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function logoutUser() {
-		const headers = { 'X-Api-Key': apiKey };
-		try {
-			const { status } = await httpClient.post(`/login/logout`, {}, { headers });
-			if (status === 204) return;
-			else throw new Error("logout failed");
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function logoutUser() {
+        const headers = { "X-Api-Key": apiKey };
+        try {
+            const { status } = await httpClient.post(`/login/logout`, {}, { headers });
+            if (status === 204) return;
+            else throw new Error("logout failed");
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function getUserStatus(userId: string) {
-		if (!userId) throw new Error("userId is required");
-		const headers = { 'X-Api-Key': apiKey };
-		try {
-			const request = () => httpClient.get(`/users/${userId}/status`, { headers });
-			const { data } = await authInterceptor<{ data: { status: string } }>(request);
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function getUserStatus(userId: string) {
+        if (!userId) throw new Error("userId is required");
+        const headers = { "X-Api-Key": apiKey };
+        try {
+            const request = () => httpClient.get(`/users/${userId}/status`, { headers });
+            const { data } = await authInterceptor<{
+                data: { status: string };
+            }>(request);
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function getQuote(payload: QuoteRequestPayload) {
-		const headers = { 'X-Api-Key': apiKey };
-		try {
-			const request = () => httpClient.post(`/quotes`, payload, { headers });
-			const { data } = await authInterceptor<{ data: TransactPayload }>(request);
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function getQuote(payload: QuoteRequestPayload) {
+        const headers = { "X-Api-Key": apiKey };
+        try {
+            const request = () => httpClient.post(`/quotes`, payload, { headers });
+            const { data } = await authInterceptor<{ data: TransactPayload }>(request);
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	async function transact(transactPayload: TransactPayload) {
-		const headers = { 'X-Api-Key': apiKey };
-		try {
-			const request = () => httpClient.post(`/transactions`, transactPayload, { headers });
-			const { data } = await authInterceptor<{ data: TransactionResponse }>(request);
-			return data;
-		} catch (e: any) {
-			const error = _getErrorFromAxiosError(e);
-			throw error;
-		}
-	}
+    async function transact(transactPayload: TransactPayload) {
+        const headers = { "X-Api-Key": apiKey };
+        try {
+            const request = () => httpClient.post(`/transactions`, transactPayload, { headers });
+            const { data } = await authInterceptor<{
+                data: TransactionResponse;
+            }>(request);
+            return data;
+        } catch (e: any) {
+            const error = _getErrorFromAxiosError(e);
+            throw error;
+        }
+    }
 
-	function _getErrorFromAxiosError(e: any) {
-		if (e.data) return e.data;
-		if (e.response) return e.response.data;
-		else if (e.request) return e.request;
-		if (e.message) return e.message;
-		return e;
-	}
+    function _getErrorFromAxiosError(e: any) {
+        if (e.data) return e.data;
+        if (e.response) return e.response.data;
+        else if (e.request) return e.request;
+        if (e.message) return e.message;
+        return e;
+    }
 
-	async function authInterceptor<T>(request: any): Promise<T> {
-		try {
-			const res = await request();
-			return res;
-		} catch (e: any) {
-			if (e.status === 401 && e.data?.code === 'MISSING_TOKEN' || e.data?.code === 'TOKEN_EXPIRED') {
-				const data = await refreshToken(_userWalletAddress);
-				if (data) {
-					// request again
-					return request();
-				}
-			}
-			throw e;
-		}
-	}
+    async function authInterceptor<T>(request: any): Promise<T> {
+        try {
+            const res = await request();
+            return res;
+        } catch (e: any) {
+            if ((e.status === 401 && e.data?.code === "MISSING_TOKEN") || e.data?.code === "TOKEN_EXPIRED") {
+                const data = await refreshToken(_userWalletAddress);
+                if (data) {
+                    // request again
+                    return request();
+                }
+            }
+            throw e;
+        }
+    }
 
-	return {
-		createApiKey,
-		getApiKeys,
-		validateApiKey,
-		requestLogin,
-		createUser,
-		updateUser,
-		requestEmailVerification,
-		loginUser,
-		refreshToken,
-		logoutUser,
-		getUserStatus,
-		getQuote,
-		transact,
-		setWalletAddress
-	};
+    return {
+        createApiKey,
+        getApiKeys,
+        validateApiKey,
+        requestLogin,
+        createUser,
+        updateUser,
+        requestEmailVerification,
+        loginUser,
+        refreshToken,
+        logoutUser,
+        getUserStatus,
+        getQuote,
+        transact,
+        setWalletAddress,
+        setApiKey,
+    };
 }
 
 export interface ApiClient {
-	createApiKey: () => Promise<{ apiKey: string }>;
-	getApiKeys: () => Promise<ApiKeyResponse[]>;
-	validateApiKey: (keyId: string) => Promise<{ Status: string }>;
-	requestLogin: (walletAddress: string) => Promise<{ nonce: string }>;
-	createUser: (nonce: string, signature: string, visitor?: VisitorData) => Promise<{ authToken: AuthToken, user: User }>;
-	updateUser: (userId: string, userUpdate: UserUpdate) => Promise<User>;
-	requestEmailVerification: (userId: string, email: string) => Promise<void>;
-	loginUser: (nonce: string, signature: string, visitor?: VisitorData) => Promise<{ authToken: AuthToken, user: User }>;
-	refreshToken: (walletAddress: string) => Promise<{ authToken: AuthToken, user: User }>;
-	logoutUser: () => Promise<void>;
-	getUserStatus: (userId: string) => Promise<{ status: string }>;
-	getQuote: (payload: QuoteRequestPayload) => Promise<TransactPayload>;
-	transact: (quote: TransactPayload) => Promise<TransactionResponse>;
-	setWalletAddress: (walletAddress: string) => void;
+    createApiKey: () => Promise<{ apiKey: string }>;
+    getApiKeys: () => Promise<ApiKeyResponse[]>;
+    validateApiKey: (keyId: string) => Promise<{ Status: string }>;
+    requestLogin: (walletAddress: string) => Promise<{ nonce: string }>;
+    createUser: (nonce: string, signature: string, visitor?: VisitorData) => Promise<{ authToken: AuthToken; user: User }>;
+    updateUser: (userId: string, userUpdate: UserUpdate) => Promise<User>;
+    requestEmailVerification: (userId: string, email: string) => Promise<void>;
+    loginUser: (nonce: string, signature: string, visitor?: VisitorData) => Promise<{ authToken: AuthToken; user: User }>;
+    refreshToken: (walletAddress: string) => Promise<{ authToken: AuthToken; user: User }>;
+    logoutUser: () => Promise<void>;
+    getUserStatus: (userId: string) => Promise<{ status: string }>;
+    getQuote: (payload: QuoteRequestPayload) => Promise<TransactPayload>;
+    transact: (quote: TransactPayload) => Promise<TransactionResponse>;
+    setWalletAddress: (walletAddress: string) => void;
+    setApiKey: (apiKey: string) => void;
 }
 
 interface ApiKeyResponse {
-	id: string;
-	status: string;
-	authType: string;
-	data: string;
-	createdAt: string;
-	updatedAt: string;
+    id: string;
+    status: string;
+    authType: string;
+    data: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 interface RefreshToken {
-	token: string;
-	expAt: Date;
+    token: string;
+    expAt: Date;
 }
 
 interface AuthToken {
-	token: string;
-	refreshToken: RefreshToken;
-	issuedAt: string;
-	expAt: string;
+    token: string;
+    refreshToken: RefreshToken;
+    issuedAt: string;
+    expAt: string;
 }
 
 interface UserUpdate {
-	walletAddress?: string;
-	firstName?: string;
-	middleName?: string;
-	lastName?: string;
+    walletAddress?: string;
+    firstName?: string;
+    middleName?: string;
+    lastName?: string;
 }
 
 export interface User {
-	id: string;
-	firstName: string;
-	middleName: string;
-	lastName: string;
-	status: string;
-	type: string;
-	tags: object;
-	createdAt: string;
-	updateAt: string;
-	email?: string;
+    id: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    status: string;
+    type: string;
+    tags: object;
+    createdAt: string;
+    updateAt: string;
+    email?: string;
 }
 
 export interface VisitorData {
-	visitorId?: string;
-	requestId?: string;
+    visitorId?: string;
+    requestId?: string;
 }
 
 export interface Quote {
-	timestamp: number;
-	baseUSD: number;
-	gasUSD: number;
-	tokenUSD: number;
-	serviceUSD: number;
-	totalUSD: number;
-	signature: string;
+    timestamp: number;
+    baseUSD: number;
+    gasUSD: number;
+    tokenUSD: number;
+    serviceUSD: number;
+    totalUSD: number;
+    signature: string;
 }
 
 export interface TransactPayload extends Quote {
-	userAddress: string;
-	chainID: number;
-	contractAddress: string;
-	contractFunction: string;
-	contractReturn: string;
-	contractParameters: string[];
-	txValue: string;
-	gasLimit: string;
-	cardToken: string;
+    userAddress: string;
+    chainID: number;
+    contractAddress: string;
+    contractFunction: string;
+    contractReturn: string;
+    contractParameters: string[];
+    txValue: string;
+    gasLimit: string;
+    cardToken: string;
 }
 
 export interface TransactionResponse {
-	txID: string;
-	txUrl: string;
+    txID: string;
+    txUrl: string;
 }
 
 export interface QuoteRequestPayload {
-	chainID: number;
-	userAddress: string;
-	contractAddress: string;
-	contractFunction: string;
-	contractReturn: string,
-	contractParameters: string[];
-	txValue: string;
-	gasLimit: string;
+    chainID: number;
+    userAddress: string;
+    contractAddress: string;
+    contractFunction: string;
+    contractReturn: string;
+    contractParameters: string[];
+    txValue: string;
+    gasLimit: string;
 }
 
 export interface ApiClientOptions {
-	apiKey: string;
-	baseUrl: string;
-	walletAddress: string;
+    baseUrl: string;
 }
---
+++ src/lib/services/events.service.ts
@@ -0,0 +1,268 @@
+import type { StringPay, StringPayload } from "../StringPay";
+import type { ApiClient, QuoteRequestPayload, TransactPayload, User } from "./apiClient.service";
+import type { AuthService } from "./auth.service";
+import type { LocationService } from "./location.service";
+import type { QuoteService } from "./quote.service";
+
+const CHANNEL = "STRING_PAY";
+const IFRAME_URL = new URL(import.meta.env.VITE_IFRAME_URL).origin;
+
+export enum Events {
+    LOAD_PAYLOAD = "load_payload",
+    IFRAME_READY = "ready",
+    IFRAME_RESIZE = "resize",
+    IFRAME_CLOSE = "close",
+    REQUEST_AUTHORIZE_USER = "request_authorize_user",
+    RECEIVE_AUTHORIZE_USER = "receive_authorize_user",
+    REQUEST_RETRY_LOGIN = "request_retry_login",
+    RECEIVE_RETRY_LOGIN = "receive_retry_login",
+    REQUEST_EMAIL_VERIFICATION = "request_email_verification",
+    RECEIVE_EMAIL_VERIFICATION = "receive_email_verification",
+    REQUEST_CONFIRM_TRANSACTION = "request_confirm_transaction",
+    RECEIVE_CONFIRM_TRANSACTION = "receive_confirm_transaction",
+    REQUEST_QUOTE_START = "request_quote_start",
+    QUOTE_CHANGED = "quote_changed",
+    REQUEST_QUOTE_STOP = "request_quote_stop",
+}
+
+const eventHandlers: Record<string, (event: StringEvent, stringPay: StringPay) => void> = {};
+
+const _handleEvent = async (e: any) => {
+    if (e.origin !== IFRAME_URL) return;
+
+    const stringPay: StringPay = (<any>window).StringPay;
+
+    try {
+        const payload = JSON.parse(e.data);
+        const channel = payload.channel;
+        const event = <StringEvent>payload.event;
+        if (channel === CHANNEL) {
+            const handler = eventHandlers[event.eventName];
+            if (handler) await handler(event, stringPay);
+            else console.debug("SDK :: Unhandled event: ", event);
+        }
+    } catch (error) {
+        console.error("sdk: _handleEvent error: ", error);
+    }
+};
+
+const registerEvents = () => {
+    unregisterEvents();
+
+    window.addEventListener("message", _handleEvent);
+};
+
+const unregisterEvents = () => {
+    window.removeEventListener("message", _handleEvent);
+};
+
+function cleanup() {
+    unregisterEvents();
+
+    if ((<any>window).StringPay) {
+        (<any>window).StringPay.frame?.remove();
+        (<any>window).StringPay.frame = undefined;
+        (<any>window).StringPay.isLoaded = false;
+        (<any>window).StringPay.onFrameClose();
+    }
+}
+
+export function createEventsService(authService: AuthService, quoteService: QuoteService, apiClient: ApiClient, locationService: LocationService) {
+    const sendEvent = <T = any>(frame: HTMLIFrameElement, eventName: string, data?: T, error?: any) => {
+        if (!frame) {
+            err("a frame was not provided to sendEvent");
+        }
+
+        const stringEvent: StringEvent = { eventName, data, error };
+        const message = JSON.stringify({
+            channel: CHANNEL,
+            event: stringEvent,
+        });
+
+        frame.contentWindow?.postMessage(message, "*");
+    };
+
+    //
+    eventHandlers[Events.IFRAME_READY] = onIframeReady;
+    eventHandlers[Events.IFRAME_CLOSE] = onIframeClose;
+    eventHandlers[Events.IFRAME_RESIZE] = onIframeResize;
+    eventHandlers[Events.REQUEST_AUTHORIZE_USER] = onAuthorizeUser;
+    eventHandlers[Events.REQUEST_RETRY_LOGIN] = onRetryLogin;
+    eventHandlers[Events.REQUEST_EMAIL_VERIFICATION] = onEmailVerification;
+    eventHandlers[Events.REQUEST_QUOTE_START] = onQuoteStart;
+    eventHandlers[Events.REQUEST_QUOTE_STOP] = onQuoteStop;
+    eventHandlers[Events.REQUEST_CONFIRM_TRANSACTION] = onConfirmTransaction;
+
+    /** -------------- EVENT HANDLERS  ---------------- */
+
+    async function onIframeReady(event: StringEvent, stringPay: StringPay) {
+        let user = null as User | null;
+
+        if (!stringPay.frame || !stringPay.payload) throw new Error("Iframe not ready");
+
+        apiClient.setWalletAddress(stringPay.payload.userAddress);
+        apiClient.setApiKey(stringPay.payload.apiKey);
+
+        // init fp service
+        locationService.getFPInstance().catch((err) => console.debug("getFPInstance error: ", err));
+
+        try {
+            user = await authService.fetchLoggedInUser(stringPay.payload.userAddress);
+        } catch (e) {
+            console.debug("fetchLoggedInUser error", e);
+        }
+
+        const iframePayload = createIframePayload(stringPay.payload, user);
+        sendEvent(stringPay.frame, Events.LOAD_PAYLOAD, iframePayload);
+        stringPay.isLoaded = true;
+        stringPay.onFrameLoad();
+    }
+
+    async function onIframeClose() {
+        console.debug("SDK :: onIframeClose");
+        quoteService.stopQuote();
+        cleanup();
+    }
+
+    async function onIframeResize(event: StringEvent, stringPay: StringPay) {
+        if (!stringPay.frame || !stringPay.payload) throw new Error("Iframe not ready");
+
+        const frame = stringPay.frame;
+
+        if (event.data?.height != frame.scrollHeight) {
+            frame.style.height = (event.data?.height ?? frame.scrollHeight) + "px";
+        }
+    }
+
+    async function onAuthorizeUser(event: StringEvent, stringPay: StringPay) {
+        if (!stringPay.frame || !stringPay.payload) throw new Error("Iframe not ready");
+
+        try {
+            const { user } = await authService.loginOrCreateUser(stringPay.payload.userAddress);
+            sendEvent(stringPay.frame, Events.RECEIVE_AUTHORIZE_USER, { user });
+        } catch (error: any) {
+            console.debug("SDK :: onAuthorizeUser error: ", error);
+            sendEvent(stringPay.frame, Events.RECEIVE_AUTHORIZE_USER, {}, error);
+        }
+    }
+
+    async function onRetryLogin(event: StringEvent, { frame }: StringPay) {
+        if (!frame) throw new Error("Iframe not ready");
+
+        try {
+            const { user } = await authService.retryLogin();
+            sendEvent(frame, Events.RECEIVE_RETRY_LOGIN, { user });
+        } catch (error) {
+            sendEvent(frame, Events.RECEIVE_RETRY_LOGIN, {}, error);
+        }
+    }
+
+    async function onEmailVerification(event: StringEvent, { frame }: StringPay) {
+        if (!frame) throw new Error("Iframe not ready");
+
+        try {
+            const data = <{ userId: string; email: string }>event.data;
+            await apiClient.requestEmailVerification(data.userId, data.email);
+            sendEvent(frame, Events.RECEIVE_EMAIL_VERIFICATION, {
+                status: "email_verified",
+            });
+        } catch (error: any) {
+            sendEvent(frame, Events.RECEIVE_EMAIL_VERIFICATION, {}, error);
+        }
+    }
+
+    async function onQuoteStart(event: StringEvent, { frame, payload }: StringPay) {
+        if (!frame) throw new Error("Iframe not ready");
+
+        const quotePayload = <QuoteRequestPayload>payload;
+
+        const callback = (quote: TransactPayload) => sendEvent(frame, Events.QUOTE_CHANGED, { quote });
+        quoteService.startQuote(quotePayload, callback);
+    }
+
+    async function onQuoteStop() {
+        quoteService.stopQuote();
+    }
+
+    async function onConfirmTransaction(event: StringEvent, { frame }: StringPay) {
+        if (!frame) throw new Error("Iframe not ready");
+
+        try {
+            const data = <TransactPayload>event.data;
+            const txHash = await apiClient.transact(data);
+            sendEvent(frame, Events.RECEIVE_CONFIRM_TRANSACTION, txHash);
+        } catch (error: any) {
+            sendEvent(frame, Events.RECEIVE_CONFIRM_TRANSACTION, {}, error);
+        }
+    }
+
+    // Subscribe to wallet change events
+    window.ethereum.removeAllListeners("accountsChanged");
+    window.ethereum.on("accountsChanged", (accounts: string[]) => {
+        apiClient.setWalletAddress(accounts[0]);
+        logout();
+        cleanup();
+        quoteService.stopQuote();
+    });
+
+    function logout() {
+        apiClient.logoutUser();
+    }
+
+    return {
+        registerEvents,
+        unregisterEvents,
+        sendEvent,
+    };
+}
+
+// Parse payload before sending it to the iframe
+function createIframePayload(payload: StringPayload, _user: User | null): IframePayload {
+    const nft: NFT = {
+        name: payload.name,
+        price: payload.price,
+        currency: payload.currency,
+        collection: payload.collection ?? "",
+        imageSrc: payload.imageSrc,
+        imageAlt: payload?.imageAlt ?? "NFT",
+    };
+
+    return {
+        nft,
+        user: {
+            walletAddress: payload.userAddress,
+            id: _user?.id ?? "",
+            status: _user?.status ?? "",
+            email: _user?.email ?? "",
+        },
+    };
+}
+
+function err(msg: string) {
+    console.error("[String Pay] " + msg);
+}
+
+export interface StringEvent {
+    eventName: string;
+    data?: any;
+    error: string;
+}
+
+export interface NFT {
+    name: string;
+    price: number;
+    currency: string;
+    collection?: string;
+    imageSrc: string;
+    imageAlt?: string;
+}
+
+export interface IframePayload {
+    nft: NFT;
+    user: {
+        id: string;
+        walletAddress: string;
+        status: string;
+        email: string;
+    };
+}
